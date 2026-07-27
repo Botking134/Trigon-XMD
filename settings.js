67e996e682f6cecf8b6f2278775f1a5ededecd07
Botking134 <167695470+Botking134@users.noreply.github.com>
2026-07-27 23:41:27 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,12 @@
+export default {
+    botName: "Trigon-XMD",
+    ownerName: "Infinity",
+    prefix: ".",
+    
+    // Secondary Master Phone Number (Plain number with country code, e.g., "2348000000000")
+    // Note: Primary Root Master is hardcoded in handlers (2347040401291)
+    masterNumber: "",
+
+    // Groq API Key (Can also be set dynamically via .setkey <gsk_...>)
+    groqApiKey: ""
+};
